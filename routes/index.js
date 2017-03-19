a31b964e18e8b10a1732babe2f18efb489c4af25
Elaine Feng <eleanor52@live.ca>
2017-03-18 23:25:40 -0700

    Connected to db using mongoose (hopefully), users.json contains user model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nwhacks2017');
+
+var userSchema = {
+    name: String,
+    food: Array
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Daily Nutrition Tracker' });
 });
 
 module.exports = router;
